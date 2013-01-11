998f4f6a8245ff57b89634e0e824d466bcf9f09a
Adam Dong <fengqiu.dong@gmail.com>
2013-01-11 17:59:19 +0800

    committing two files

---
+++ Smart Monitor/fdsfd.h
@@ -0,0 +1,13 @@
+//
+//  fdsfd.h
+//  Smart Monitor
+//
+//  Created by Adam dong on 11/01/13.
+//  Copyright (c) 2013 Service-Indeed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface fdsfd : NSObject
+
+@end
