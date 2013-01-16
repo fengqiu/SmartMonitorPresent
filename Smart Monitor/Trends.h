12312a284d6abcc6d97be1c8d2679fcc3c22a774
Adam Dong <fengqiu.dong@gmail.com>
2013-01-16 16:48:58 +0800

    Made new file, trends for the graph view.

---
+++ Smart Monitor/Trends.h
@@ -0,0 +1,14 @@
+//
+//  Trends.h
+//  Smart Monitor
+//
+//  Created by Adam dong on 16/01/13.
+//  Copyright (c) 2013 Service-Indeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Trends : UIViewController
+@property (strong, nonatomic) IBOutlet CPTGraphHostingView *GraphView;
+
+@end
---
+++ Smart Monitor/Trends.m
@@ -0,0 +1,38 @@
+//
+//  Trends.m
+//  Smart Monitor
+//
+//  Created by Adam dong on 16/01/13.
+//  Copyright (c) 2013 Service-Indeed. All rights reserved.
+//
+
+#import "Trends.h"
+
+@interface Trends ()
+
+@end
+
+@implementation Trends
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
