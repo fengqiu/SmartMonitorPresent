1e1bffae91b1af9d225797bfe4cbea625c1f391e
Adam Dong <fengqiu.dong@gmail.com>
2013-01-16 14:06:13 +0800

    forgot to change branch

--- Smart Monitor/DetailViewController.m
+++ Smart Monitor/DetailViewController.m
@@ -16,23 +16,34 @@
 
 #pragma mark - Managing the detail item
 
+
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    //return number of rows, webservice.count??
+    return 1;
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+-(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+}
+
 - (void)setDetailItem:(id)newDetailItem
 {
-    if (_detailItem != newDetailItem) {
-        _detailItem = newDetailItem;
-        
-        // Update the view.
-        [self configureView];
-    }
+  
 }
 
 - (void)configureView
 {
     // Update the user interface for the detail item.
 
-    if (self.detailItem) {
-        self.detailDescriptionLabel.text = [self.detailItem description];
-    }
+
 }
 
 - (void)viewDidLoad
@@ -41,7 +52,7 @@
 	// Do any additional setup after loading the view, typically from a nib.
     [self configureView];
     
-    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg_cork.png"]];
+
 }
 
 - (void)didReceiveMemoryWarning
